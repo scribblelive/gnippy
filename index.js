6da73bb9993faf6e7754bba74ca12d6bc17fd826
Michael De Lorenzo <michael@delorenzodesign.com>
2014-10-14 17:45:34 -0400

    Adds rules management.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 module.exports  = {
-  Powertrack: require('./lib/gnippy/powertrack/powertrack_stream')
+  Powertrack: {
+    Stream: require('./lib/gnippy/powertrack/powertrack_stream'),
+    Rules:  require('./lib/gnippy/rules/powertrack_rules')
+  }
 };
