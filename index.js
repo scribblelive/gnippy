b981743de066193ac438d3810b7ed0fb13b87647
Michael De Lorenzo <michael@delorenzodesign.com>
2014-10-21 13:54:42 -0400

    Adds support for the Compliance API.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports  = {
   Powertrack: {
+    ComplianceAPI: require('./lib/gnippy/compliance/compliance_api'),
     Stream: require('./lib/gnippy/powertrack/powertrack_stream'),
     Rules:  require('./lib/gnippy/rules/powertrack_rules')
   }
