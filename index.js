4aeb2db86f50e75914a7c90bf651c84fd3ea51ea
Michael De Lorenzo <michael@delorenzodesign.com>
2014-10-22 17:25:43 -0400

    Adds Twitter search support.

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ module.exports  = {
     Stream: require('./lib/gnippy/powertrack/powertrack_stream'),
     Replay: require('./lib/gnippy/powertrack/powertrack_replay'),
     Rules:  require('./lib/gnippy/rules/powertrack_rules')
+  },
+  Search:{
+    Twitter:  require('./lib/gnippy/search/twitter_search')
   }
 };
