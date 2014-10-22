274134497dfb3d960e50e626a1fdc31d28b3999a
Michael De Lorenzo <michael@delorenzodesign.com>
2014-10-22 12:42:48 -0400

    Adds Powertrack replay.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports  = {
   Powertrack: {
     ComplianceAPI: require('./lib/gnippy/compliance/compliance_api'),
     Stream: require('./lib/gnippy/powertrack/powertrack_stream'),
+    Replay: require('./lib/gnippy/powertrack/powertrack_replay'),
     Rules:  require('./lib/gnippy/rules/powertrack_rules')
   }
 };
